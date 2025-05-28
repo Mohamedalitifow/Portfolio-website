764db132f8dd2be7576db495416fb5425cb67888
Mohamedalitifow <maxameds486@gmail.com>
2025-05-28 11:12:01 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+   <div className='text-5xl text-slate-900 hover:text-orange-700 hover:text-7xl'> welcome mali</div>
+   
+   
+  )
+}
+
+export default App
